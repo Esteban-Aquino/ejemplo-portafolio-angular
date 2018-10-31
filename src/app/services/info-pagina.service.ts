c9289fa4858f739089822684b1ae6af2923c2678
Esteban Aquino <eaquinof@gmail.com>
2018-10-31 16:49:46 -0500

    Fin seccion 5

--- src/app/services/info-pagina.service.ts
+++ src/app/services/info-pagina.service.ts
@@ -1,21 +1,36 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { InfoPagina } from '../interfaces/info-pagina..interface';
+import { InfoPagina } from '../interfaces/info-pagina.interface';
+import { Equipo } from '../interfaces/equipo.ingerface';
 
 @Injectable({
   providedIn: 'root'
 })
 export class InfoPaginaService {
   info: InfoPagina = {};
+  equipo: Equipo[] = [{}];
   cargada = false;
   constructor(private http: HttpClient) {
+    this.cargarInfo ();
+    this.cargarEquipo ();
     console.log('Servicio Listo....');
+   }
+   private cargarInfo () {
     // Leer archivo Json
     this.http.get('assets/data/data-pagina.json')
              .subscribe((resp: InfoPagina) => {
                this.cargada = true;
                this.info = resp;
-                console.log(resp);
+                // console.log(resp);
+             });
+   }
+   private cargarEquipo () {
+    // Leer archivo Json
+    this.http.get('https://ejemplo-angular-hmtl.firebaseio.com/eaquino.json')
+             .subscribe((resp: Equipo[]) => {
+               this.cargada = true;
+               this.equipo = resp;
+                // console.log(resp);
              });
    }
 }
